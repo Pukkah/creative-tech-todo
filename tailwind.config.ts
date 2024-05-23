2a2f7ebe8da77aad56a08acba347bcebcfd96c51
Miks Ramāns <miksramans@gmail.com>
2024-05-23 12:54:09 +0300

    add rubik webfont

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 import type { Config } from "tailwindcss";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
