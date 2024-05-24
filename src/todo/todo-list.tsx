ad9d0aba386377a5b37b0ea6f438a41584de6598
Miks Ramāns <miksramans@gmail.com>
2024-05-24 13:31:03 +0300

    feat: animate todo list changes

--- src/todo/todo-list.tsx
+++ src/todo/todo-list.tsx
@@ -1,11 +1,16 @@
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { TodoListItem } from "./todo-list-item";
 import { useTodoStore } from "./todo.store";
 
 export const TodoList = () => {
   const { todos, updateStatus } = useTodoStore();
+  const [animateRef] = useAutoAnimate();
 
   return (
-    <ul className="flex flex-col gap-1.5 rounded-2xl empty:aspect-square empty:animate-pulse empty:bg-black/5">
+    <ul
+      className="flex flex-col gap-1.5 rounded-2xl empty:aspect-square empty:animate-pulse empty:bg-black/5"
+      ref={animateRef}
+    >
       {todos?.map((todo) => (
         <TodoListItem
           key={todo.id}
